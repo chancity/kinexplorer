ac7e639803f829a20c13cb26a958c012c788bb89
chancity <hawbakerchancey@gmail.com>
2018-12-11 19:24:42 -0800

    Update Statistics.js

--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -7,7 +7,7 @@ class Statistics extends React.Component {
 			<div class="row">
 				<div class="panel panel-default">
 					<div class="panel-body">
-						<iframe src="https://grafana.kinexplorer.com/d/Wv3AjfDik/kin-evolution-stats?orgId=1" width="100%" height="1600px" frameborder="0">
+						<iframe src="https://grafana.kinexplorer.com/d/m0QvPkEmk/aggregated-stats?orgId=1" width="100%" height="1600px" frameborder="0">
 						</iframe>
 					</div>
 				</div>
